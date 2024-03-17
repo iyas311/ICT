3a3f3e27a50f59a1419657aecfe101b4639d1795
iyas311 <70274015+iyas311@users.noreply.github.com>
2024-03-17 09:56:38 +0530

    Update summary.py

--- summary.py
+++ summary.py
@@ -1,19 +1,11 @@
 from pysummarization.nlpbase.auto_abstractor import AutoAbstractor
 from pysummarization.tokenizabledoc.simple_tokenizer import SimpleTokenizer
 from pysummarization.abstractabledoc.top_n_rank_abstractor import TopNRankAbstractor
-# Object of automatic summarization.
 document = input("ENTER THE DOCUMENT")
-
 auto_abstractor = AutoAbstractor()
-
 auto_abstractor.tokenizable_doc = SimpleTokenizer()
-
 auto_abstractor.delimiter_list = [".", "\n"]
-
 abstractable_doc = TopNRankAbstractor()
-
 result_dict = auto_abstractor.summarize(document, abstractable_doc)
-
-
 for sentence in result_dict["summarize_result"]:
     print(sentence)
